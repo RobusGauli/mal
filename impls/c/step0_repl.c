dbf3f26ceb60a0f6aeee5e9f245025ca35cddfa4
Robus Gauli <robusgauli@gmail.com>
2021-09-13 17:52:26 +0545

    Add result type to handle EOF case

--- impls/c/step0_repl.c
+++ impls/c/step0_repl.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef char* string;
 
@@ -20,24 +21,52 @@ string rep(string expr) {
   return PRINT(EVAL(READ(expr)));
 }
 
-string get_input(FILE* file) {
-  char* buffer = NULL;
+typedef struct {
+  string value;
+  bool ok;
+} string_result_t;
+
+string_result_t string_result__new(string value, bool ok) {
+  return ((string_result_t){.value=value, .ok=ok});
+}
+
+void strip_new_lines_if_exists(char* buffer, size_t len) {
+  char ch = ((buffer)[len-1]);
+  if (ch == '\n') {
+    buffer[len-1] = 0;
+  }
+}
+
+string_result_t get_input(FILE* file) {
+  string buffer = NULL;
   size_t buffer_cap;
   size_t len = getline(&buffer, &buffer_cap, file);
-  buffer[len-1] = 0; // strip new line
-  return buffer;
+  if (len == -1) {
+    // EOF
+    return string_result__new(buffer, false);
+  }
+  // strips out new line character if exists
+  strip_new_lines_if_exists(buffer, len);
+  return string_result__new(buffer, true);
 }
 
 int main() {
   for(;;) {
     printf("user> ");
-    string input = get_input(stdin);
-    if (strlen(input))  {
-      printf("%s\n", rep(input));
+    string_result_t result__input = get_input(stdin);
+
+    if (!result__input.ok) {
+      goto cleanup;
     }
-    if (input) {
-      free(input);
-    }
+
+    printf("%s\n",  result__input.value);
+
+    free(result__input.value);
+    continue;
+
+cleanup:
+    free(result__input.value);
+    break;
   }
-  return 0;
+
 }
