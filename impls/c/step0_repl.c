d58cbbd3cd92c4fc8bcc1496f9d6263902a82d58
Robus Gauli <robusgauli@gmail.com>
2021-09-13 18:31:08 +0545

    Refactor mal impl

--- impls/c/step0_repl.c
+++ impls/c/step0_repl.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 #include <stdbool.h>
+
 
 typedef char* string;
 
@@ -24,13 +26,14 @@ string rep(string expr) {
 typedef struct {
   string value;
   bool ok;
+  bool is_eof;
 } string_result_t;
 
-string_result_t string_result__new(string value, bool ok) {
-  return ((string_result_t){.value=value, .ok=ok});
+string_result_t string_result__new(string value, bool ok, bool is_eof) {
+  return ((string_result_t){.value=value, .ok=ok, .is_eof=is_eof});
 }
 
-void strip_new_lines_if_exists(char* buffer, size_t len) {
+void strip_new_line_if_exists(char* buffer, size_t len) {
   char ch = ((buffer)[len-1]);
   if (ch == '\n') {
     buffer[len-1] = 0;
@@ -43,30 +46,61 @@ string_result_t get_input(FILE* file) {
   size_t len = getline(&buffer, &buffer_cap, file);
   if (len == -1) {
     // EOF
-    return string_result__new(buffer, false);
+    if (errno == 0) {
+      return string_result__new(buffer, false, true);
+    }
+    return string_result__new(buffer, false, false);
   }
   // strips out new line character if exists
-  strip_new_lines_if_exists(buffer, len);
-  return string_result__new(buffer, true);
+  strip_new_line_if_exists(buffer, len);
+  return string_result__new(buffer, true, false);
+}
+
+bool string_result__iseof(string_result_t* result) {
+  return result -> is_eof;
+}
+
+bool string_result__ok(string_result_t* result) {
+  return result -> ok;
+}
+
+
+string string_result__val(string_result_t* result) {
+  return result -> value;
+}
+
+void string_result__free(string_result_t* result) {
+  free(result -> value);
+}
+
+size_t string_result__len(string_result_t* result) {
+  return strlen(result -> value);
 }
 
 int main(void) {
   for(;;) {
     printf("user> ");
-    string_result_t result__input = get_input(stdin);
+    string_result_t result = get_input(stdin);
 
-    if (!result__input.ok) {
-      goto cleanup;
+    if (string_result__iseof(&result)) {
+      string_result__free(&result);
+      return 0;
     }
 
-    printf("%s\n",  result__input.value);
+    if (!string_result__ok(&result)) {
+      string_result__free(&result);
+      perror("unable to read line");
+      return 1;
+    }
 
-    free(result__input.value);
-    continue;
+    if (!string_result__len(&result)) {
+      continue;
+      string_result__free(&result);
+    }
 
-cleanup:
-    free(result__input.value);
-    break;
+    printf("%s\n", string_result__val(&result));
+    printf("len is: %ld\n", string_result__len(&result));
+    string_result__free(&result);
   }
 
 }
