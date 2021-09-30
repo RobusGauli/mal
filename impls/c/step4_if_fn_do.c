a35122d4f60feabe716a755777316c4b4c78af90
Robus Gauli <robusgauli@gmail.com>
2021-09-30 11:27:36 +0545

    Format stuff

--- impls/c/step4_if_fn_do.c
+++ impls/c/step4_if_fn_do.c
@@ -1,40 +1,36 @@
 #define _POSIX_C_SOURCE 200809L
-#include <stdio.h>
 #include <readline/history.h>
 #include <readline/readline.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 typedef struct {
-}  Mal;
+} Mal;
 
-Mal m_read(const char* input) {
-  return (Mal){};
-}
+Mal m_read(const char *input) { return (Mal){}; }
 
-Mal m_eval(Mal mal) {
-  return (Mal){};
-}
+Mal m_eval(Mal mal) { return (Mal){}; }
 
-char* m_print(Mal mal) {
-  char* src = "echo world";
-  char* sample_string = malloc((sizeof(char) * strlen(src)) + (sizeof(char)));
+char *m_print(Mal mal) {
+  char *src = "echo world";
+  char *sample_string = malloc((sizeof(char) * strlen(src)) + (sizeof(char)));
   memcpy(sample_string, src, strlen(src));
   sample_string[strlen(src)] = '\0';
   return sample_string;
 }
 
-#define forever for (;;)
-
 int main() {
 
-  forever {
-    char* input = readline("user>");
+  for (;;) {
+    char *input = readline("user>");
 
-    if (input == NULL) exit(0);
+    if (input == NULL) {
+      exit(0);
+    };
 
     Mal mal = m_read(input);
-    Mal result =  m_eval(mal);
-    char* stringified = m_print(result);
+    Mal result = m_eval(mal);
+    char *stringified = m_print(result);
     printf("%s\n", stringified);
     //
     // free
